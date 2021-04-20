216ab9f049e1cf6e26e781d4ab4d5eaccc0e2366
Vishnu Sayanth A V <46415790+vishnusayanth@users.noreply.github.com>
2021-04-20 13:33:28 +0530

    Update Rovers.js

--- src/nasa_src/Rovers.js
+++ src/nasa_src/Rovers.js
@@ -3,17 +3,17 @@ import React from 'react';
 
 function Rovers(props) {
     return (     
-        <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div className="row row-cols-1 row-cols-md-3 g-4">
              {props.roversList.map(rover =>        
-                  <div key={rover} class="col">
-                    <div class="card h-100">
-                      <img src={'../../assets/img/rovers/'+rover + '.jpg'} class="card-img-top" alt={rover}>
-                      <div class="card-body">
-                        <h5 class="card-title">Mars Rover : {rover}</h5>
-                        <p class="card-text">Click below to view images.</p>
+                  <div key={rover} className="col">
+                    <div className="card h-100">
+                      <img src={'../../assets/img/rovers/'+rover + '.jpg'} className="card-img-top" alt={rover}>
+                      <div className="card-body">
+                        <h5 className="card-title">Mars Rover : {rover}</h5>
+                        <p className="card-text">Click below to view images.</p>
                       </div>
                       <div class="card-footer">
-                        <small class="text-muted" onClick={()=>props.selectFunction(rover)}>Click me!</small>
+                        <small className="text-muted" onClick="{()=>props.selectFunction(rover)}">Click me</small>
                       </div>
                     </div>
                   </div>  
